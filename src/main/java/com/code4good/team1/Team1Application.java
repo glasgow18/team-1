11f99c2a4f4881b12ff4a9ef7861aad2864b8792
Darren Lowe <yxb15183@strath.ac.uk>
2018-10-27 15:59:31 +0100

    Finally, git be gone

---
+++ src/main/java/com/code4good/team1/Team1Application.java
@@ -0,0 +1,12 @@
+package com.code4good.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1Application.class, args);
+	}
+}
---
+++ src/main/java/com/code4good/team1/api/actuator.java
@@ -0,0 +1,24 @@
+package com.code4good.team1.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class actuator {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public String sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return "hello " + name;
+    }
+
+
+}

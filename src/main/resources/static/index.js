c234dd8c9a7561f456b3b79aee4975fb1f799d66
Scott <mjb15165@uni.strath.ac.uk>
2018-10-27 20:57:01 +0100

    Im sorry i done more files than i was meant to my baaaad

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,10 @@
+function loadRecentActivities(){
+    var recentActivitiesList = document.getElementById("recentActivitiesList");
+
+    recentActivitiesList.innerHTML = "<ul>" +
+        "<li>Leaf</li>" +
+        "<li>2 Leafs</li>" +
+        "</ul>";
+}
+
+

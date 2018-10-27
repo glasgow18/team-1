c385da4fffb07051b29509acd1bec17126bcc073
Gavin Craig <gavinci@Gavins-MacBook-Pro.local>
2018-10-27 23:06:03 +0100

    we searchin bois

--- team-1-web-app/src/App.js
+++ team-1-web-app/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Login from './components/login.js';
+import SearchPage from './components/searchpage.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <SearchPage/>
       </div>
     );
   }
---
+++ team-1-web-app/src/components/activity.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './searchpage.css';
+class Activity extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name:'Test Name',
+      description:'Test description',
+      image:'gnomechild.jpg',
+      maplocation:'',
+      lat:0,
+      long:0,
+      comments:'',
+      id:'',
+      tags:'',
+    };
+  }
+    render(){
+        return (
+        <div className="col-sm-12">
+        <div className="card">
+          <div className="card-body">
+            <div className="jumbotron">
+              <div className="row">
+                <div className="col-sm-6"><img src={this.state.image} width="90%" height="90%"/></div>
+                <div className="col-sm-4"><h3>{this.state.name}</h3></div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12">
+                <p className="lead">{this.state.description}</p>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        );
+    }
+}
+
+export default Activity
